d0ed9b6b7b96ea74272f8e466b17a701620fd322
qianshuang.7 <qianshuang.7@bytedance.com>
2026-09-07 23:24:35 +0800

    feat: add ClipMind connected-people app icon
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: I62fa8bdd3e3f1ee154528366c83db0ee97022d7b

--- android/app/src/main/java/com/clipmind/android/ui/SettingsScreen.kt
+++ android/app/src/main/java/com/clipmind/android/ui/SettingsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
